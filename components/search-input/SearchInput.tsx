8c9769529e78b678d204216f0d65b2cc809b14c2
Jay Lee <jaylee.possible@gmail.com>
2022-03-26 22:43:45 +0900

    Change search field style

--- components/search-input/SearchInput.tsx
+++ components/search-input/SearchInput.tsx
@@ -1,14 +1,13 @@
 import { SearchRounded } from '@mui/icons-material'
 import styles from './SearchInput.module.scss'
 
-export interface SearchInputProps
-  extends Omit<React.HTMLProps<HTMLInputElement>, 'type'> {}
+export interface SearchInputProps extends Omit<React.HTMLProps<HTMLInputElement>, 'type'> {}
 
 const SearchInput = (props: SearchInputProps) => {
   return (
     <label className={styles.label}>
       <SearchRounded className={styles.icon} />
-      <input className={styles.input} type="search" {...props} />
+      <input className={styles.input} {...props} />
     </label>
   )
 }
