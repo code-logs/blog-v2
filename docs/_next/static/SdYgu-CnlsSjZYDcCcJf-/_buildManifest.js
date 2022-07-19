141f9ca6ad0bd1c8aeb1b270b9fe07ffc2753249
possible819 <possible819@users.noreply.github.com>
2022-07-19 23:56:39 +0000

    Commit from GitHub Actions (Docs)

--- docs/_next/static/SdYgu-CnlsSjZYDcCcJf-/_buildManifest.js
+++ docs/_next/static/SdYgu-CnlsSjZYDcCcJf-/_buildManifest.js
@@ -4,7 +4,7 @@
     '/': [s, 'static/css/e7e3f8470cce38e3.css', 'static/chunks/pages/index-0b3c74e9142164e3.js'],
     '/404': ['static/chunks/pages/404-0242deb77f513299.js'],
     '/_error': ['static/chunks/pages/_error-2280fa386d040b66.js'],
-    '/about': ['static/css/95ce1b29e3fb4366.css', 'static/chunks/pages/about-9632d04c794756e6.js'],
+    '/about': ['static/css/95ce1b29e3fb4366.css', 'static/chunks/pages/about-4cd8200ff5da2b61.js'],
     '/categories/[category]/[page]': ['static/css/08145f571dff66c9.css', 'static/chunks/pages/categories/[category]/[page]-296a0f93b151ac53.js'],
     '/licenses': ['static/css/6eff21aad43a8d4e.css', 'static/chunks/pages/licenses-24c8ee29a1c55808.js'],
     '/posts/[page]': [s, 'static/css/257bb9ee60cca3a9.css', 'static/chunks/pages/posts/[page]-094c0cc37443049c.js'],
