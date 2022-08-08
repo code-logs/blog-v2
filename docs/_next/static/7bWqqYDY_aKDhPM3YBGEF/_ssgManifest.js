7750ea44595a0d1da3dbeac4c15d48cc35d2d1e2
possible819 <possible819@users.noreply.github.com>
2022-08-08 00:08:16 +0000

    Commit from GitHub Actions (Docs)

---
+++ docs/_next/static/7bWqqYDY_aKDhPM3YBGEF/_ssgManifest.js
@@ -0,0 +1,2 @@
+self.__SSG_MANIFEST = new Set(['\u002Ftags', '\u002F', '\u002F[title]', '\u002Fcategories\u002F[category]\u002F[page]', '\u002Fposts\u002F[page]'])
+self.__SSG_MANIFEST_CB && self.__SSG_MANIFEST_CB()
