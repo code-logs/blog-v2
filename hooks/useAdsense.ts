3da04e46e4c30a1e4dc9737b9bf794c958f5b934
Jay Lee <jaylee.possible@gmail.com>
2022-08-13 03:00:36 +0900

    fix: change ad client id

--- hooks/useAdsense.ts
+++ hooks/useAdsense.ts
@@ -1,16 +1,16 @@
 import { useEffect } from 'react'
 import blogConfig from '../config/blog.config'
 
-const useAdsense = () => {
+const useAdsense = (adClient: typeof blogConfig.googleAdsense.adClient) => {
   useEffect(() => {
     if (document.head) {
       const script = document.createElement('script')
       script.async = true
-      script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${blogConfig.googleAdsense.mainBannerAdSlot}`
+      script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adClient}`
       script.crossOrigin = 'anonymous'
       document.head.appendChild(script)
     }
-  }, [])
+  }, [adClient])
 }
 
 export default useAdsense
