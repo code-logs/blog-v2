10c1ae471271c366a4b17d64f07a6fc91633e037
Jay Lee <jaylee.possible@gmail.com>
2023-05-01 23:28:52 +0900

    feat: add cat bread banner

---
+++ config/banner.config.ts
@@ -0,0 +1,11 @@
+import { CarouselBannerProps } from '../components/carousel-banner'
+
+const bannerConfig: CarouselBannerProps['banners'] = [
+  {
+    title: '고양이 빵 Cat bread - 세마리의 고양이와 홈베이킹 이야기',
+    bannerSrc: '/assets/banners/cat-bread.webp',
+    link: 'https://m.youtube.com/@cat_bread.',
+  },
+]
+
+export default bannerConfig
